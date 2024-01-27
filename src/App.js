40866813af6ee9d2f4ca84d8d33548be8cf5ed90
Kev <kevinheyland@icloud.com>
2024-01-27 20:38:05 +0100

    Update Prettier configuration and format scripts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Hello World")
-  );
-};
+    return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'))
+}
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement(App))
