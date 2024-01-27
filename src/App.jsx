fc070a4c4ef31ed126003739b0ff7331f32aa61e
Kev <kevinheyland@icloud.com>
2024-01-27 21:19:08 +0100

    Add Vite scripts and update ReactDOM import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom'
 
 const App = () => {
     return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'))
 }
 
 const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
+const root = createRoot(container)
 root.render(React.createElement(App))
